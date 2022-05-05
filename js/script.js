696433e9b82a1b4ea2df3ff33958e9512f3a13b8
grisha <sshaprans@gmail.com>
2022-05-05 14:14:51 +0300

    reworking FormData on json placeholder

--- js/script.js
+++ js/script.js
@@ -238,38 +238,36 @@ document.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
+// request.setRequestHeader('Content-type', 'multipart/form-data');
+// в комбі XMLHttpRequest і Data заголовок встановлюється автоматично
 
-            // request.setRequestHeader('Content-type', 'multipart/form-data');
-            // в комбі XMLHttpRequest і Data заголовок встановлюється автоматично
-
-            // для формата json, треба заголовок FormData треба зробити об'єктом
+// для формата json, треба заголовок FormData треба зробити об'єктом
 
             const formData = new FormData(form);
+         
             const obj = {};
             formData.forEach(function(value, key){
                 obj[key] = value;
             });
 
-            const json = JSON.stringify(obj);
-
-
-            request.setRequestHeader('Content-type', 'application/json');
-
-            
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if(request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove()
-                } else {
-                    showThanksModal(message.failure);
-                }
-            });
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(obj)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                form.reset();
+                statusMessage.remove()
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            })
 
         });
     }
@@ -295,5 +293,5 @@ document.addEventListener('DOMContentLoaded', () => {
            closeModal();
         }, 4500);
     }
-    
+
 });
